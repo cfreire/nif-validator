7278d1745b740887d81b3174d4e3844f10317bd3
Cesar Freire <cfreire@noware.eu>
2026-09-04 14:51:56 +0100

    Add bug on nif_validator

--- nif_validator.py
+++ nif_validator.py
@@ -13,7 +13,7 @@ def valida_nif(numero):
     if not numero.isdigit() or len(numero) != EXPECTED_DIGITS: 
         return False
     soma = sum([int(dig) * (EXPECTED_DIGITS - pos) for pos, dig in enumerate(numero)])
-    resto = soma % 11
+    resto = soma % 10
     if (numero[-1] == '0' and resto == 1):
         resto = (soma + 10) % 11
     return resto == 0
